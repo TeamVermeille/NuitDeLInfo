b52f3723226da823da6f14986565a0f6d717a622
KLEIN Paul <paul.klein@epita.fr>
2014-12-05 07:11:19 +0100

    [+] New post

--- post.php
+++ post.php
@@ -39,8 +39,6 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 echo json_encode($resultat);
-<<<<<<< HEAD
-=======
 #echo $firstname;
 #echo $lastname;
 #echo $age;
@@ -48,4 +46,3 @@ echo json_encode($resultat);
 #echo $event;
 #echo $location;
 #echo $state;
->>>>>>> [+] Last version
